c99e6226f4d49f6bf80bd5f5121eecfa1dbf063e
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-06-25 14:45:03 -0400

    fonts

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+*= require fonts
 
 require 'rails/all'
 
@@ -10,7 +11,8 @@ module FierceStories
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.paths << Rails.root.join('lib', 'assets', 'fonts')
+    config.assets.precompile += %w(.svg .eot .woff .ttf)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
