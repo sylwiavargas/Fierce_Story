877707e431b6e65a9977d4ee665bd556047f94f3
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-06-24 20:24:00 -0400

    debugging

--- db/migrate/20190508215147_add_character_id_to_story_books.rb
+++
@@ -1,5 +0,0 @@
-class AddCharacterIdToStoryBooks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :story_books, :character_id, :integer
-  end
-end
--- db/migrate/20190510134349_remove_comment_id_from_story_books.rb
+++
@@ -1,4 +0,0 @@
-class RemoveCommentIdFromStoryBooks < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
